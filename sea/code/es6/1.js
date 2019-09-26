5862c85d2209092f1c6d73d3f98167157526acf9
root <root@localhost.localdomain>
2019-09-26 22:58:44 +0800

    Sass页面完成，写es6

--- sea/code/es6/1.js
+++ sea/code/es6/1.js
@@ -63,10 +63,52 @@ function f(){
 		}
 }
 f();*/
+/*
 
 var s = 'hello';
 
 for (var i=0; i<s.length; i++){
 	console.log(s[i]);
 }
-console.log(i);
+console.log(i);*/
+
+/*
+function f1(){
+	let n = 5;
+	if (true) {
+		let n = 10;
+	}
+	console.log(n);
+}
+f1();
+*/
+
+/*
+{{{{
+	{let insane = 'Hello World'}
+	console.log(insane);
+}}}}*/
+
+/*
+{{{{
+	let insane = 'Hello World';
+	{let insane = 'Hello World'}
+}}}}*/
+
+/*
+function f() {
+	console.log('I am outside');
+}
+*/
+
+/*
+(function () {
+	if (false){
+		function f() {
+			console.log('I am outside');
+		}
+	}
+})
+*/
+
+
