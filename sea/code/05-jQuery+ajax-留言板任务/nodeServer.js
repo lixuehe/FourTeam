d2719cda64021d7314f82060f8ae1556b3508bf6
root <root@localhost.localdomain>
2019-10-16 17:03:49 +0800

    删除功能完成90%，差一步渲染

--- sea/code/05-jQuery+ajax-留言板任务/nodeServer.js
+++ sea/code/05-jQuery+ajax-留言板任务/nodeServer.js
@@ -38,8 +38,38 @@ var server = http.createServer(function (request, response) {
                 postData += chunk;
             })
             request.on("end",function(){
-                //如果postData有数据，则执行添加留言函数。
-                if(postData.length==0){
+                //如果postData的长度等于0，则退出执行
+                if(postData.length===0){
+                    return false;
+                }
+                // 如果传入的参数是1个，则认为是 传递了删除参数
+                if(postData.length===1){
+                    // 将要删除的那个参数赋值 给 deleteId，并postData设置为空
+                    let deleteId = postData;
+                    postData = "";
+                    console.log("要删除的值是: "+deleteId);
+                    fs.readFile('./text.json',function(err,data){
+                        if(err){
+                            return console.error(err);
+                        }
+                        var jsonStr = data.toString();//数据 转换为json字符串
+                        var json = JSON.parse(jsonStr);//json字符串转换为json对象
+                        //从json字符串中遍历，如果找到了要删除的那个值，就从数组里删除它
+                        for(var i=0; i<json.message.length; i++){
+                            if(json.message[deleteId]===json.message[i]){
+                                json.message.splice(deleteId,1);
+                            }
+                        }
+                        var str = JSON.stringify(json);//json对象转换为字符串
+                        console.log(str);
+                        fs.writeFile('./text.json',str,{flag:'w'},err=>{
+                            if(!err){
+                                console.log("数据删除成功");
+                            }else{
+                                console.log("数据删除失败");
+                            }
+                        })
+                    })
                     return false;
                 }
                 addMessage()
@@ -75,6 +105,11 @@ function addMessage(){
             }
         })
     })
+}
+
+// 删除留言函数
+function deleteMessage(){
+
 }
 
 server.listen(8080);
