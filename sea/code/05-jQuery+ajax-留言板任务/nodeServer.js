c0c579be64f9eee28ce8a03a38eaa56ff27e5dec
root <root@localhost.localdomain>
2019-10-16 14:58:57 +0800

    备份一份代码

--- sea/code/05-jQuery+ajax-留言板任务/nodeServer.js
+++ sea/code/05-jQuery+ajax-留言板任务/nodeServer.js
@@ -13,6 +13,10 @@ var
 // 从命令行参数获取root目录，默认是当前目录:
 var root = path.resolve(process.argv[2] || '.');
 
+// 接收post参数。
+var postData = "";
+
+
 console.log('Static root dir: ' + root);
 
 // 创建服务器:
@@ -30,34 +34,15 @@ var server = http.createServer(function (request, response) {
             response.writeHeader(200, {'Content-Type' : 'text/html;charset:utf-8'});
                 // 将文件流导向response:
             fs.createReadStream(filepath).pipe(response);
-            // 接收post参数。
-            var postData = "";
             request.on("data",function(chunk){
                 postData += chunk;
             })
             request.on("end",function(){
+                //如果postData有数据，则执行添加留言函数。
                 if(postData.length==0){
                     return false;
                 }
-                fs.readFile('./text.json',function(err,data){
-                  if(err){
-                      return console.error(err);
-                  }
-                  var jsonStr = data.toString();//将二进制转为json字符串
-                  var json = JSON.parse(jsonStr);//将json字符串转换为json对象
-                  var post = JSON.parse(postData);//传进来的post数据是json字符串，转换为json对象
-                  json.message.push(post);
-                  console.log(json.message);
-                  var str = JSON.stringify(json);//json对象转换为json字符串
-                    fs.writeFile('./text.json',str,{flag:'w'},err=>{
-                        if(!err){
-                            console.log('数据写入成功');
-                            console.log(postData);
-                        }else{
-                            console.log('数据写入失败');
-                        }
-                    })
-                })
+                addMessage()
             })
         } else {
             // 出错了或者文件不存在:
@@ -68,8 +53,28 @@ var server = http.createServer(function (request, response) {
         }
     });
 });
-function testJson(){
-
+// 添加留言函数
+function addMessage(){
+    fs.readFile('./text.json',function(err,data){
+        if(err){
+            return console.error(err);
+        }
+        var jsonStr = data.toString();// 将二进制转为json字符串
+        var json = JSON.parse(jsonStr);// 将json字符串转换为json对象
+        var post = JSON.parse(postData);// 传进来的post数据是json字符串，转换为json对象
+        json.message.push(post);
+        var str = JSON.stringify(json);// json对象转换为json字符串
+        fs.writeFile('./text.json',str,{flag:'w'},err=>{
+            if(!err){
+                console.log('数据写入成功');
+                console.log("postData:"+postData);
+                // 数据添加成功后，postData重置为空。
+                postData = "";
+            }else{
+                console.log('数据写入失败');
+            }
+        })
+    })
 }
 
 server.listen(8080);
